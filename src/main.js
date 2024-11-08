995923d3587874e496b2bddaefc02b36ecbce77d
Rui <yehruei@163.com>
2024-11-08 17:20:36 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import './style.css'
 import App from './App.vue'
 import router from './router/router'
 import naive from 'naive-ui'
-import 'vfonts/IBMPlexSans.css'
 
 createApp(App).use(naive).use(router).mount('#app')
