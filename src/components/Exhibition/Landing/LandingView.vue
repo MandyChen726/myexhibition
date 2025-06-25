acbde6d181945b3f3c1fb4ef9b41b69dfa7ad7bc
Rui <yehruei@163.com>
2025-06-25 10:36:49 +0800

    Create LandingView.vue

---
+++ src/components/Exhibition/Landing/LandingView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+    <n-layout>
+        <n-layout-sider style="height: 100vh">
+            <div class="rotated-text">
+                竖排文字示例
+            </div>
+        </n-layout-sider>
+        <n-layout-content>
+
+        </n-layout-content>
+    </n-layout>
+</template>
+
+<style lang='less' scoped>
+.rotated-text {
+    transform: rotate(90deg);
+    transform-origin: top left;
+    position: relative;
+    top: 1rem;
+    left: 1rem;
+}
+</style>
